3221b0d76bb59dc3244a74930039650221f8023e
Trinhdinhlong <trinhdinhlong2003@gmail.com>
2025-04-22 17:57:12 +0700

    update controller

--- src/main/java/com/example/user_service/controllers/UserController.java
+++ src/main/java/com/example/user_service/controllers/UserController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 import com.example.user_service.request.LoginRequest;
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin("*")
 public class UserController {
     @Autowired
     private UserService userService;
